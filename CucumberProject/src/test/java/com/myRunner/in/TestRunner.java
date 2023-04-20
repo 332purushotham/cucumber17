33fc0794928f5ad3ac1147379444e3c85efdc961
332purushotham <purushothaminfo71@gmail.com>
2023-04-20 10:55:06 +0530

    4 20 23

--- CucumberProject/src/test/java/com/myRunner/in/TestRunner.java
+++ CucumberProject/src/test/java/com/myRunner/in/TestRunner.java
@@ -1,5 +1,20 @@
 package com.myRunner.in;
 
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+  
+   features = "/CucumberProject/src/test/resources/Features/logging.feature",
+		   
+   //path of step definition file
+		   glue={"LoggingClass"},
+				    
+				//   glue= {"Steps"},
+				   tags = "@SmokeTest" 
+   )
 public class TestRunner {
 
 }
